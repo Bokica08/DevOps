8e41d014ec777d023694b061dc4ce6bb2ab4d03b
Bojan <bojantrpeski123@gmail.com>
2023-06-28 19:23:22 +0200

    Bug fix

--- dians-front-end/src/app/_services/auth.service.ts
+++ dians-front-end/src/app/_services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {environment} from '../enviroment/enviroment'
 
-const AUTH_API = environment.apiBaseUrl+'api/auth/';
+const AUTH_API = environment.apiBaseUrl+'/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json'})
